aedfd876a116c29732b07766f411ddae58188288
Thomas262761 <6293254@cegepmontpetit.ca>
2025-11-16 19:39:33 -0500

    part 4

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,5 +1,6 @@
 import Icons from './utils/Icons.js';
-
+import Swiper from 'swiper/bundle';
+Icons.load();
 class Main {
   constructor() {
     this.init();
@@ -7,6 +8,18 @@ class Main {
 
   init() {
     Icons.load();
+    this.initPagination();
+  }
+
+  initPagination() {
+    const target = document.querySelector('.js-pagination');
+    const swiper = new Swiper(target, {
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-naviguation',
+      },
+    });
   }
 }
 new Main();
